3cfd00cac90bd69ed14fb349bd3089f64e99210d
gabriel-liz <gabriel.liz@outlook.com.br>
2026-02-19 21:56:33 -0300

    Criado projetos grade base * Adicionado o plug-in idea no arquivo build.grade para exibir java doc e source(Dessa forma exibe a documentacao)

---
+++ src/main/java/com/avaliacomentarios/moderacao/ModeracaoApplication.java
@@ -0,0 +1,13 @@
+package com.avaliacomentarios.moderacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModeracaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModeracaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avaliacomentarios/moderacao/ModeracaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avaliacomentarios.moderacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModeracaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
